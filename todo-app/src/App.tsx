4fdb5dd618144dfafc3467fae0178e269dbb35df
Julia Robinson <julia.robinson@spareroom.co.uk>
2023-06-08 17:01:12 +0100

    basic todo input

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,5 +1,28 @@
-import styles from './styles/app.module.css';
+// import styles from './styles/app.module.css';
+import {useState, useRef} from 'react';
 
-const App = () => <h1 className={styles.class1}>Hello</h1>
+type Todo = {
+  text: string
+}
+
+const App = () => {
+  const todoInputRef = useRef<HTMLInputElement>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!todoInputRef.current?.value.trim().length) {
+      return;
+    }
+
+    setTodos([...todos, {text: todoInputRef.current.value}]);
+    todoInputRef.current.value = '';
+  }
+
+  return (<form onSubmit={handleSubmit}>
+    <input ref={todoInputRef} type="text"></input>
+    <button>Submit</button>
+  </form>)
+}
 
 export default App;
