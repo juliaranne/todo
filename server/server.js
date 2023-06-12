72cab4fbb8b90cc803e236b6328f5221fa7bb3f8
Julia Robinson <julia.robinson@spareroom.co.uk>
2023-06-12 16:22:08 +0100

    connect to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express"); //import express
 const app = express();
 const routes = require("./routes/todo");
+const mongoose = require("mongoose");
 app.use(express.json());
 
 app.use("/", routes);
@@ -8,3 +10,13 @@ app.use("/", routes);
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("App is listening on port " + listener.address().port);
 });
+
+//establish connection to database
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
